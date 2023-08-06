c51749b531db4b2f65302dda3c022be7977a5516
saptarshithe42 <saptarshi.the.42@gmail.com>
2023-08-06 17:05:33 +0530

    minor changes

--- src/components/CourseGrid.jsx
+++ src/components/CourseGrid.jsx
@@ -1,32 +1,31 @@
-import React from "react"
+import React from "react";
 
 //styles
-import "./CourseGrid.css"
+import "./CourseGrid.css";
 
 // components
-import CourseCard from "./CourseCard"
-import DraftCard from "./DraftCard"
-import PublishedCard from "./PublishedCard"
-import PurchasedCard from "./PurchasedCard"
+import CourseCard from "./CourseCard";
+import DraftCard from "./DraftCard";
+import PublishedCard from "./PublishedCard";
+import PurchasedCard from "./PurchasedCard";
 
-function CourseGrid({courseList, cardType}) {
-  return (
-    <div className="book-grid-holder container">
-
-      <div className="book-grid row justify-content-center">
-        {courseList.map((course, index) => {
-          if(cardType === "draft")
-            return (<DraftCard course={course} key={index} />)
-          else if (cardType === "course")
-            return (<CourseCard course={course} key={index}  />)
-          else if(cardType === "published")
-            return <PublishedCard course={course} key={index}  />
-          else if(cardType === "purchased")
-            return (<PurchasedCard course={course} key={index} />)
-        })}
-      </div>
-    </div>
-  )
+function CourseGrid({ courseList, cardType }) {
+    return (
+        <div className="course-grid-holder container">
+            <div className="course-grid row justify-content-center">
+                {courseList.map((course, index) => {
+                    if (cardType === "draft")
+                        return <DraftCard course={course} key={index} />;
+                    else if (cardType === "course")
+                        return <CourseCard course={course} key={index} />;
+                    else if (cardType === "published")
+                        return <PublishedCard course={course} key={index} />;
+                    else if (cardType === "purchased")
+                        return <PurchasedCard course={course} key={index} />;
+                })}
+            </div>
+        </div>
+    );
 }
 
-export default CourseGrid
+export default CourseGrid;
--- src/pages/search/SearchResults.jsx
+++ src/pages/search/SearchResults.jsx
@@ -1,49 +1,45 @@
-import React from "react"
-import { useParams } from "react-router-dom"
-import { projectFirestore } from "../../firebase/config"
-import { useEffect, useState } from "react"
+import React from "react";
+import { useParams } from "react-router-dom";
+import { projectFirestore } from "../../firebase/config";
+import { useEffect, useState } from "react";
 
 // styles
-import "./SearchResults.css"
-
+import "./SearchResults.css";
 
 // components
-import LoadingAnimation from "../../components/LoadingAnimation"
+import LoadingAnimation from "../../components/LoadingAnimation";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import CourseGrid from "../../components/CourseGrid"
+import CourseGrid from "../../components/CourseGrid";
 
 function SearchResults() {
+    const { query } = useParams();
 
-    const { query } = useParams()
-
-    const [courseList, setCourseList] = useState([])
-    const [fetchLimit, setFetchLimit] = useState(1)
-    const [isLoading, setIsLoading] = useState(true)
+    const [courseList, setCourseList] = useState([]);
+    const [fetchLimit, setFetchLimit] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
 
     const fetchQueryData = async () => {
-
         try {
-            const booksRef = projectFirestore.collection("courses")
+            const courseRef = projectFirestore.collection("courses");
 
-            let docs = await booksRef
+            let docs = await courseRef
                 .where("isPublished", "==", true)
                 .where("name", ">=", query)
                 .where("name", "<=", query + "\uf8ff")
-                .limit(fetchLimit).get()
+                .limit(fetchLimit)
+                .get();
 
-            let arr = []
+            let arr = [];
             docs.forEach((doc) => {
                 // doc.data()
-                arr.push({ ...doc.data(), id: doc.id })
-                console.log(doc.data())
+                arr.push({ ...doc.data(), id: doc.id });
+                console.log(doc.data());
+            });
 
-            })
-
-            setCourseList(arr)
-            setIsLoading(false)
+            setCourseList(arr);
+            setIsLoading(false);
         } catch (err) {
-
             // alert(err)
 
             console.log(err.message);
@@ -57,30 +53,25 @@ function SearchResults() {
                 draggable: true,
                 progress: undefined,
                 theme: "light",
-            })
+            });
         }
-
-    }
+    };
 
     useEffect(() => {
-
-        fetchQueryData()
-
-    }, [fetchLimit])
-
+        fetchQueryData();
+    }, [fetchLimit]);
 
     const incrementFetchLimit = () => {
-
         setFetchLimit((prev) => {
-            return (prev + 5);
-        })
-
-    }
+            return prev + 5;
+        });
+    };
 
     return (
         <div>
-            {isLoading ? <LoadingAnimation /> :
-
+            {isLoading ? (
+                <LoadingAnimation />
+            ) : (
                 <div className="main-div">
                     <ToastContainer
                         position="top-center"
@@ -94,19 +85,21 @@ function SearchResults() {
                         pauseOnHover={false}
                         theme="light"
                     />
-                    <h1 className="list-heading">Search Results for "{query}"</h1>
-                    <CourseGrid
-                        courseList={courseList}
-                        cardType="course"
-                    />
+                    <h1 className="list-heading">
+                        Search Results for "{query}"
+                    </h1>
+                    <CourseGrid courseList={courseList} cardType="course" />
 
-                    <button className="btn btn-primary"
+                    <button
+                        className="btn btn-primary"
                         onClick={incrementFetchLimit}
-                    >More</button>
+                    >
+                        More
+                    </button>
                 </div>
-            }
+            )}
         </div>
-    )
+    );
 }
 
-export default SearchResults
+export default SearchResults;
